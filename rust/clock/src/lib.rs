71d6d69973bfa11be3ed2315dac8eeb4096377fd
Kevin Németh <nkevin@outlook.hu>
2023-12-26 13:46:59 +0100

    Finish rust clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,34 @@
-pub struct Clock;
+use std::fmt::Display;
+
+const MINUTES_PER_DAY: i32 = MINUTES_PET_HOUR * 24;
+const MINUTES_PET_HOUR: i32 = 60;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock {
+            minutes: (hours * MINUTES_PET_HOUR + minutes).rem_euclid(MINUTES_PER_DAY),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Clock {
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_PER_DAY),
+        }
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes / MINUTES_PET_HOUR,
+            self.minutes % MINUTES_PET_HOUR
+        )
     }
 }
