ac7b497e3f8c40838907b0a53fa05de8e50d8704
Kevin Németh <nkevin@outlook.hu>
2023-12-28 07:57:30 +0100

    Finish rust minesweeper

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,41 @@
+static NEIGBOUR_CELL_OFFSETS: &'static [(i8, i8)] = &[
+    (-1, -1),   (0, -1),    (1, -1),
+    (-1, 0),                (1, 0),
+    (-1, 1),    (0, 1),     (1, 1),
+];
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let height = minefield.len();
+    if height == 0 {
+        return minefield.iter().map(|&s| s.to_string()).collect();
+    }
+    let width = minefield[0].len();
+    (0..height)
+        .map(|y| {
+            (0..width)
+                .map(|x| {
+                    let cell = minefield[y].as_bytes()[x];
+                    if cell == b'*' {
+                        '*'
+                    } else {
+                        match NEIGBOUR_CELL_OFFSETS
+                            .iter()
+                            .map(|(ox, oy)| (x as i8 + ox, y as i8 + oy))
+                            .filter(|&(x, y)| {
+                                x >= 0 && x < width as i8 && y >= 0 && y < height as i8
+                            })
+                            .filter(|&(x, y)| {
+                                let cell = minefield[y as usize].as_bytes()[x as usize];
+                                cell == b'*'
+                            })
+                            .count()
+                        {
+                            0 => ' ',
+                            n => char::from_digit(n as u32, 10).unwrap(),
+                        }
+                    }
+                })
+                .collect()
+        })
+        .collect()
 }
