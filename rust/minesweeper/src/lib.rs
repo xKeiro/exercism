48bac1ff017d9d3f7935b4cc6aaed842e286bb53
Kevin Németh <nkevin@outlook.hu>
2023-12-30 09:22:02 +0100

    Refactor rust minefield annotation logic

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,41 +1,48 @@
-static NEIGHBOUR_CELL_OFFSETS: &'static [(i8, i8)] = &[
-    (-1, -1),   (0, -1),    (1, -1),
-    (-1, 0),                (1, 0),
-    (-1, 1),    (0, 1),     (1, 1),
-];
-
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let height = minefield.len();
-    if height == 0 {
-        return minefield.iter().map(|&s| s.to_string()).collect();
-    }
-    let width = minefield[0].len();
-    (0..height)
-        .map(|y| {
-            (0..width)
-                .map(|x| {
-                    let cell = minefield[y].as_bytes()[x];
-                    if cell == b'*' {
-                        '*'
-                    } else {
-                        match NEIGHBOUR_CELL_OFFSETS
-                            .iter()
-                            .map(|(ox, oy)| (x as i8 + ox, y as i8 + oy))
-                            .filter(|&(x, y)| {
-                                x >= 0 && x < width as i8 && y >= 0 && y < height as i8
-                            })
-                            .filter(|&(x, y)| {
-                                let cell = minefield[y as usize].as_bytes()[x as usize];
-                                cell == b'*'
-                            })
-                            .count()
-                        {
-                            0 => ' ',
-                            n => char::from_digit(n as u32, 10).unwrap(),
-                        }
-                    }
-                })
+    minefield
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.as_bytes()
+                .iter()
+                .enumerate()
+                .map(|(x, &cell)| get_annotation(cell, minefield, x, y))
                 .collect()
         })
         .collect()
+}
+
+fn get_annotation(cell: u8, minefield: &[&str], x: usize, y: usize) -> char {
+    if cell == b'*' {
+        '*'
+    } else {
+        let neighbours = get_neighbours(minefield, x, y);
+        match get_neighbour_mine_count(&neighbours) {
+            0 => ' ',
+            n => char::from_digit(n as u32, 10).unwrap(),
+        }
+    }
+}
+
+fn get_neighbours<'a>(minefield: &'a [&'a str], x: usize, y: usize) -> Vec<&u8> {
+    let width = minefield[0].len();
+    let height = minefield.len();
+    let neighbors = (-1..=1)
+        .flat_map(|xi| (-1..=1).map(move |yi| (xi, yi)))
+        .filter(|&(xi, yi)| !(xi == 0 && yi == 0))
+        .filter_map(|(xi, yi)| {
+            let new_x = x as i8 + xi;
+            let new_y = y as i8 + yi;
+            if new_x < 0 || new_x >= width as i8 || new_y < 0 || new_y >= height as i8 {
+                None
+            } else {
+                Some(&minefield[new_y as usize].as_bytes()[new_x as usize])
+            }
+        })
+        .collect();
+    neighbors
+}
+
+fn get_neighbour_mine_count(neighbours: &[&u8]) -> usize {
+    neighbours.iter().filter(|&&cell| *cell == b'*').count()
 }
