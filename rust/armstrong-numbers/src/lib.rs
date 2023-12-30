3ccf61689f081e161490447459be77e38da6c58f
Kevin Németh <nkevin@outlook.hu>
2023-12-30 15:23:00 +0100

    Refactor rust armstrong_number

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let num_str = num.to_string();
     let num_digits = num_str.len() as u32;
-    let mut digits = num_str.chars().map(|c| c.to_digit(10).unwrap());
-    let sum = digits.try_fold(0_u32, |acc, digit| acc.checked_add(digit.pow(num_digits)));
-    matches!(sum, Some(sum) if sum == num)
+    num_str
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .try_fold(0_u32, |acc, digit| acc.checked_add(digit.pow(num_digits)))
+        .is_some_and(|sum| sum == num)
 }
