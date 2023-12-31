f0abc4f0bd3364e23571783d8081814cc18a79f2
Kevin Németh <nkevin@outlook.hu>
2023-12-31 01:22:14 +0100

    Refactor rust minesweeper to prevent unnecessary allocation

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -16,21 +16,20 @@ fn get_annotation(cell: u8, minefield: &[&str], x: usize, y: usize) -> char {
     if cell == b'*' {
         '*'
     } else {
-        let neighbours = get_neighbours(minefield, x, y);
-        match get_neighbour_mine_count(&neighbours) {
+        match get_neighbour_mine_count(get_neighbours(minefield, x, y)) {
             0 => ' ',
             n => char::from_digit(n as u32, 10).unwrap(),
         }
     }
 }
 
-fn get_neighbours<'a>(minefield: &'a [&'a str], x: usize, y: usize) -> Vec<&u8> {
+fn get_neighbours<'a>(minefield: &'a [&'a str], x: usize, y: usize) -> impl Iterator<Item = &u8> {
     let width = minefield[0].len();
     let height = minefield.len();
     (-1..=1)
         .flat_map(|xi| (-1..=1).map(move |yi| (xi, yi)))
         .filter(|&(xi, yi)| !(xi == 0 && yi == 0))
-        .filter_map(|(xi, yi)| {
+        .filter_map(move |(xi, yi)| {
             let new_x = x as i8 + xi;
             let new_y = y as i8 + yi;
             if new_x < 0 || new_x >= width as i8 || new_y < 0 || new_y >= height as i8 {
@@ -39,9 +38,8 @@ fn get_neighbours<'a>(minefield: &'a [&'a str], x: usize, y: usize) -> Vec<&u8> 
                 Some(&minefield[new_y as usize].as_bytes()[new_x as usize])
             }
         })
-        .collect()
 }
 
-fn get_neighbour_mine_count(neighbours: &[&u8]) -> usize {
-    neighbours.iter().filter(|&&cell| *cell == b'*').count()
+fn get_neighbour_mine_count<'a>(neighbours: impl Iterator<Item = &'a u8>) -> usize {
+    neighbours.filter(|&&cell| cell == b'*').count()
 }
