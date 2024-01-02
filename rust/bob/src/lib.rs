243fc8a0b4cacae615958a149a71df01a5c5a72e
Kevin Németh <nkevin@outlook.hu>
2024-01-02 19:50:15 +0100

    Finish rust bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,37 @@
 pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+    let trimmed_message = message.trim();
+
+    if trimmed_message.is_empty() {
+        return Bob::Empty.say();
+    }
+    let have_letters = message.chars().filter(|x| x.is_alphabetic()).count() > 0;
+    let is_uppercase = trimmed_message.to_uppercase() == trimmed_message && have_letters;
+    let is_question = trimmed_message.ends_with('?');
+
+    match (is_uppercase, is_question) {
+        (true, true) => Bob::YelledQuestion.say(),
+        (true, false) => Bob::Yell.say(),
+        (false, true) => Bob::Question.say(),
+        _ => Bob::Default.say(),
+    }
+}
+
+enum Bob {
+    Question,
+    Yell,
+    YelledQuestion,
+    Empty,
+    Default,
+}
+
+impl Bob {
+    fn say(&self) -> &str {
+        match *self {
+            Bob::Question => "Sure.",
+            Bob::Yell => "Whoa, chill out!",
+            Bob::YelledQuestion => "Calm down, I know what I'm doing!",
+            Bob::Empty => "Fine. Be that way!",
+            Bob::Default => "Whatever.",
+        }
+    }
 }
