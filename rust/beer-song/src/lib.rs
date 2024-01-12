cd794579173235c830df86793443e8b09d288200
Kevin Németh <nkevin@outlook.hu>
2024-01-12 20:19:48 +0100

    Finish rust beer-song

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn verse(n: u32) -> String {
-    todo!("emit verse {n}")
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\n\
+                           Go to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\n\
+                           Take it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\n\
+                           Take one down and pass it around, 1 bottle of beer on the wall.\n"),
+        _ => format!("{n} bottles of beer on the wall, {n} bottles of beer.\n\
+                      Take one down and pass it around, {n_minus_one} bottles of beer on the wall.\n",
+                     n = n,
+                     n_minus_one = n - 1),
+        
+    }
 }
 
+
 pub fn sing(start: u32, end: u32) -> String {
-    todo!("sing verses {start} to {end}, inclusive")
+    (end..=start).rev().map(verse).collect::<Vec<String>>().join("\n")
 }
