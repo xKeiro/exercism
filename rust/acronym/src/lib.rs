d7c7127837df5bf0c07280bf5a1d5129de8a1f2f
Kevin Németh <nkevin@outlook.hu>
2024-01-13 21:24:13 +0100

    Refactor rust acronym abbreviation logic

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn abbreviate(phrase: &str) -> String {
     phrase
         .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
-        .flat_map(|word| {
-            word.chars().take(1).chain(
-                word.chars()
-                    .skip_while(|c| c.is_uppercase())
-                    .filter(|c| c.is_uppercase()),
-            )
-        })
+        .flat_map(|word| word.chars().take(1).chain(extract_camelcase_chars(word)))
         .collect::<String>()
         .to_ascii_uppercase()
+}
+
+fn extract_camelcase_chars(word: &str) -> impl Iterator<Item = char> + '_ {
+    word.chars()
+        .skip_while(|c| c.is_uppercase())
+        .filter(|c| c.is_uppercase())
 }
