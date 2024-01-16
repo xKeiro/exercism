df7da44e963832f40355ad7035791986c2f38ee7
Kevin Németh <nkevin@outlook.hu>
2024-01-16 18:27:47 +0100

    Finish rust leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    match year {
+        _ if (year % 4 == 0) && (year % 100 != 0) => true,
+        _ if year % 400 == 0 => true,
+        _ => false,
+    }
 }
